603c055891e5fabe5304f81be9bf48b628fe97d2
SA64 <119963802+SA64ds@users.noreply.github.com>
2025-02-04 13:36:07 -0500

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -13,7 +13,7 @@ class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '1.0'; // This is also used for Discord RPC
 	public static var pSliceVersion:String = '2.2.2'; 
-	public static var lowbitMayhemVersion = '1.2'
+	public static var lowbitMayhemVersion = '1.2';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
